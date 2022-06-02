594cd3caadd4d0d016034e2616d057d39bc82efe
Ellawangari <elamaina8@gmail.com>
2022-06-02 19:31:57 +0000

    updated main.tf

--- PBL/main.tf
+++ PBL/main.tf
@@ -1,32 +1,3 @@
- #Variables
- 
- variable "region" {
-        default = "us-east-1"
-    }
-
-    variable "vpc_cidr" {
-        default = "172.16.0.0/16"
-    }
-
-    variable "enable_dns_support" {
-        default = "true"
-    }
-
-    variable "enable_dns_hostnames" {
-        default ="true" 
-    }
-
-    variable "enable_classiclink" {
-        default = "false"
-    }
-
-    variable "enable_classiclink_dns_support" {
-        default = "false"
-    }
-
-  variable "preferred_number_of_public_subnets" {
-  default = 2
-}
 
   #Providers
     provider "aws" {
